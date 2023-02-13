db7843fac2f09ea2230f392333c27d732cfd084b
Ali <anix1@kent.edu>
2023-02-13 16:45:02 -0700

    add state for statement/message upon button click

--- src/App.js
+++ src/App.js
@@ -1,38 +1,57 @@
-import React from 'react';
-import './App.css';
-import { affirmations, mantras } from './data.js';
+import React, { useState } from "react";
+import "./App.css";
+import { affirmations, mantras } from "./data.js";
+// import meditation from  "./assets/meditation.png";
 
 function App() {
+  const [selected, setSelected] = useState("affirmation");
+  const [statement, setStatement] = useState('🛎️');
+
+  const randomGenerator = (array) => {
+    return array[Math.floor(Math.random() * array.length)];
+  };
+
+  const radioChange = (event) => {
+    setSelected(event.target.value);
+  };
+
+  const generateStatement = () => {
+    selected === "affirmation"
+      ? setStatement(randomGenerator(affirmations))
+      : setStatement(randomGenerator(mantras));
+  };
+
   return (
     <div>
       <h1 className="title">Self Care Center</h1>
       <h2 className="sub-title">Which type of message?</h2>
       <section className="form-box">
-        <input 
-        type="radio"
-        value="affirmation"
-        id="affirmation"
-        className="radio-button"
+        <input
+          name="care-type"
+          type="radio"
+          value="affirmation"
+          id="affirmation"
+          defaultChecked={selected === "affirmation" ? true : false}
+          onChange={radioChange}
+          className="radio-button"
         />
-        <label 
-        forhtml="affirmation"
-        >
-          Affirmation
-         </label>
-         <input 
-        type="radio"
-        value="affirmation"
-        id="affirmation"
-        className="radio-button"
+        <label forhtml="affirmation">Affirmation</label>
+        <input
+          name="care-type"
+          type="radio"
+          value="mantra"
+          id="mantra"
+          defaultChecked={selected === "mantra" ? true : false}
+          onChange={radioChange}
+          className="radio-button"
         />
-        <label 
-        forhtml="affirmation"
-        >
-          Affirmation
-         </label>
+        <label forhtml="mantra">Mantra</label>
+        <button onClick={generateStatement} className="btn">
+          Click
+        </button>
       </section>
       <section className="info-box">
-        <p>{affirmations[0]}</p>
+        <p>{statement}</p>
       </section>
     </div>
   );
