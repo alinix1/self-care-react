9f2071293cb71584ad501fda6e057edbf344eaa1
Ali <anix1@kent.edu>
2023-02-13 14:38:07 -0700

    add JSX

--- src/App.js
+++ src/App.js
@@ -3,7 +3,35 @@ import './App.css';
 
 function App() {
   return (
-    <div>hello!</div>
+    <div>
+      <h1 className="title">Self Care Center</h1>
+      <h2 className="sub-title">Which type of message?</h2>
+      <section className="form-box">
+        <input 
+        type="radio"
+        value="affirmation"
+        id="affirmation"
+        className="radio-button"
+        />
+        <label 
+        forhtml="affirmation"
+        >
+          Affirmation
+         </label>
+         <input 
+        type="radio"
+        value="affirmation"
+        id="affirmation"
+        className="radio-button"
+        />
+        <label 
+        forhtml="affirmation"
+        >
+          Affirmation
+         </label>
+      </section>
+      <section className="info-box"></section>
+    </div>
   );
 }
 
